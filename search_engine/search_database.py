5688346fbf2358626c29570e6398d354da73b968
RobotStudio Automation - creating signals <przemyslaw.niedziela98@gmail.com>
2024-08-10 23:00:25 +0200

    location display

--- search_engine/search_database.py
+++ search_engine/search_database.py
@@ -1,9 +1,25 @@
+import re 
+from typing import List, Dict, Any
 from whoosh.index import open_dir
 from whoosh.qparser import MultifieldParser, FuzzyTermPlugin
-from whoosh.query import Query
-from whoosh.searching import Results
-from typing import List, Dict, Any
 
+
+def extract_data_from_io_dep(io_dep_text: str) -> Dict[str, List[str]]:
+    """
+    Extract parcel numbers and locations from the IO department section of a land registry document.
+
+    Args:
+        io_dep_text (str): The text from the IO department section of the land registry document.
+
+    Returns:
+        Dict[str, List[str]]: A dictionary containing list of parcel numbers and locations
+    """
+    first_occurrence = lambda t: t[0] if len(t) > 0 else ""
+    parcel_numbers = re.findall(r"Numer działki \d+", io_dep_text)
+    location_pattern = r"Położenie \(numer porządkowy / województwo, gmina, miejscowość\).*?(\d+ )?"
+    locations = re.findall(location_pattern, io_dep_text)
+    return {'number': first_occurrence(parcel_numbers), 
+            'location': first_occurrence(locations)}
 
 def search(query_str: str, index_dir: str = 'indexdir', limit: int = None) -> List[Dict[str, Any]]:
     """
@@ -21,12 +37,17 @@ def search(query_str: str, index_dir: str = 'indexdir', limit: int = None) -> Li
     
     with ix.searcher() as searcher:
         query_parser = MultifieldParser(
-            ["numer_ksiegi", "typ_ksiegi", "oznaczenie_wydzialu", "polozanie", "wlasciciel"],
+            ["numer_ksiegi", "typ_ksiegi", "oznaczenie_wydzialu", "polozanie", "wlasciciel", "dzial_i_o"],
             ix.schema
         )
         query_parser.add_plugin(FuzzyTermPlugin()) 
-        query: Query = query_parser.parse(query_str)
+        query = query_parser.parse(query_str)
 
-        results: Results = searcher.search(query, limit=limit)
-
-        return [dict(result) for result in results]
+        results = searcher.search(query, limit=limit)
+        
+        extended_results = []
+        for result in results: 
+            result = dict(result)
+            result.update(extract_data_from_io_dep(result['dzial_i_o']))
+            extended_results.append(result)
+        return extended_results
